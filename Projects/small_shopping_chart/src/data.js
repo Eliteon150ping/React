c6bf35eabb9b24427faad626feee5d39c1661521
Eliteon150ping <rishensurjoobhalee@gmail.com>
2024-08-03 21:32:26 +0200

    Reuploaded

---
+++ Projects/small_shopping_chart/src/data.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Gaming Spec PC',
+        price: 40000,
+        image: 'https://m.media-amazon.com/images/I/71IemWpJF4L.jpg',
+      },
+      {
+        id: '2',
+        name: 'Toyota Supra A80',
+        price: 800000,
+        image: 'https://cdn.c-magazine.com/uploads/article/2023-06-09/anastase-maragos-lrfuy93-hac-unsplash_21-48-51-021086.jpg',
+      },
+      {
+        id: '3',
+        name: 'Nike Air Jordan 4 Military Blue',
+        price: 4100,
+        image: 'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2023%2F12%2Fair-jordan-4-military-blue-fv5029-141-release-04.jpg?cbr=1&q=90',
+      },
+    ],
+  };
+  export default data;
+  
