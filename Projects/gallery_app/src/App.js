536a4bbb44263a588258a9f530ecab1096529063
Eliteon150ping <rishensurjoobhalee@gmail.com>
2024-08-03 21:31:47 +0200

    Reuploaded

---
+++ Projects/gallery_app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Gallery from "./portfolio-gallery/Gallery";
+
+function App() {
+  return (
+    <div className="container">
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
